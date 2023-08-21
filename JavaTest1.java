a5b547884d0654a77a31973f4868aa2b4b3a159a
yangrui <812474913@qq.com.com>
2023-08-21 10:06:42 +0800

    myversion4.0

--- JavaTest1.java
+++ JavaTest1.java
@@ -1,4 +1,4 @@
-public class JavaTest {
+public class JavaTest1 {
 	public static void main(String[] args) {
 		System.out.println("HelloWorld!");
 	}
--- JavaTest3.java
+++ JavaTest3.java
@@ -1,4 +1,4 @@
-public class JavaTest {
+public class JavaTest3 {
 	public static void main(String[] args) {
 		System.out.println("HelloWorld!");
 	}
